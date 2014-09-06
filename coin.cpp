bdecc0c91ddb3534d00cfe1ab603d88a82c4c4b8
pigpenguin <ethan@rooke.org>
2014-09-05 20:18:46 -0700

    Cleaed up coin.cpp

--- coin.cpp
+++ coin.cpp
@@ -7,15 +7,15 @@ int main(){
     cout << "How many pennies do you have: ";
     cin >> cents;
     // Calculating quarters
-    q = (cents-(cents%25))/25;
+    q = cents/25
     cents = cents%25;
     
     // Calculating dimes
-    d = (cents-(cents%10))/10;
+    d = cents/10;
     cents = cents%10;
 
     // Calculatin nickles
-    n = (cents-(cents%5))/5;
+    n = cents/5;
     cents = cents%5;
     
     cout << "You need\n" << "Quarters: " << q << "\nDimes: " << d << "\nNickles: " << n << "\nPennies: " << cents << endl;
