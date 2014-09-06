c5f7297ca939892979a75d2a7c67a4d9bb0da301
pigpenguin <ethan@rooke.org>
2014-09-05 20:40:00 -0700

    added correct plural handeling

--- coin.cpp
+++ coin.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main(){
     int cents, q, d, n;
+    string output;
     cout << "How many pennies do you have: ";
     cin >> cents;
     // Calculating quarters
-    q = cents/25
+    q = cents/25;
     cents = cents%25;
     
     // Calculating dimes
@@ -18,6 +19,50 @@ int main(){
     n = cents/5;
     cents = cents%5;
     
-    cout << "You need\n" << "Quarters: " << q << "\nDimes: " << d << "\nNickles: " << n << "\nPennies: " << cents << endl;
+    //Building the output string
+    output = "You have ";
+
+    if(q > 0){
+        output += q;
+        if(q == 1){
+            output += " quarter ";
+        }
+        else{
+            output += " quarters ";
+        }
+    }
+    
+    if(d > 0){
+        output += d;
+        if(d == 1){
+            output += " dime ";
+        }
+        else{
+            output += " dimes ";
+        }
+    }
+
+    if(n > 0){
+        output += n;
+        if(n == 1){
+            output += " nickle ";
+        }
+        else{
+            output += " nickles ";
+        }
+    }
+
+    if(cents > 0){
+        output += cents;
+        if(cents == 1){
+            output += " penny";
+        }
+        else{
+            output += " pennies";
+        }
+    }
+
+    cout << output << endl;
 
 }
+
